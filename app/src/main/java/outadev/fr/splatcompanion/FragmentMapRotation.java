d00f1a90f522090d3ebfcdfbd52a9669d89a67bf
outadoc <outadoc@gmail.com>
2015-08-22 00:14:10 +0200

    Dynamic map display for everybody \o/

--- app/src/main/java/outadev/fr/splatcompanion/FragmentMapRotation.java
+++ app/src/main/java/outadev/fr/splatcompanion/FragmentMapRotation.java
@@ -7,19 +7,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import outadev.fr.splatcompanion.model.GameMode;
+import outadev.fr.splatcompanion.model.Schedule;
+import outadev.fr.splatcompanion.model.Stage;
 
 /**
  * Created by outadoc on 21/08/15.
  */
-public class FragmentMapRotation extends Fragment {
+public abstract class FragmentMapRotation extends Fragment {
 
 	protected Typeface typeface;
+	protected Schedule schedule;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		typeface = Typeface.createFromAsset(getContext().getAssets(), "project_paintball_beta_2.otf");
+		schedule = (Schedule) getArguments().getSerializable("schedule");
 	}
 
 	@Nullable
@@ -30,7 +37,28 @@ public class FragmentMapRotation extends Fragment {
 		TextView txtGameMode = (TextView) view.findViewById(R.id.game_mode_title);
 		txtGameMode.setTypeface(typeface);
 
+		ImageView imgGameModeIcon = (ImageView) view.findViewById(R.id.game_mode_icon);
+		imgGameModeIcon.setImageResource(getActiveMode().getIconResId());
+
+		ViewGroup stagesContainer = (ViewGroup) view.findViewById(R.id.stages_container);
+		stagesContainer.removeAllViews();
+
+		for(Stage stage : getActiveMode().getStages()) {
+			View stageView = inflater.inflate(R.layout.stage_card, stagesContainer, false);
+
+			TextView txtMapName = (TextView) stageView.findViewById(R.id.txt_map_title);
+			ImageView imgMapPreview = (ImageView) stageView.findViewById(R.id.img_stage);
+
+			txtMapName.setTypeface(typeface);
+
+			txtMapName.setText(stage.getNameResId());
+			imgMapPreview.setImageResource(stage.getPreviewResId());
+			stagesContainer.addView(stageView);
+		}
+
 		return view;
 	}
+
+	protected abstract GameMode getActiveMode();
 
 }
--- app/src/main/java/outadev/fr/splatcompanion/FragmentRankedBattles.java
+++ app/src/main/java/outadev/fr/splatcompanion/FragmentRankedBattles.java
@@ -1,8 +1,33 @@
 package outadev.fr.splatcompanion;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import outadev.fr.splatcompanion.model.GameMode;
+
 /**
  * Created by outadoc on 21/08/15.
  */
 public class FragmentRankedBattles extends FragmentMapRotation {
+
+	@Override
+	protected GameMode getActiveMode() {
+		return schedule.getRankedMode();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+
+		TextView txtGameModeTitle = (TextView) view.findViewById(R.id.game_mode_title);
+		txtGameModeTitle.setText(schedule.getRankedMode().getGameRules().getNameResId());
+
+		return view;
+	}
 
 }
