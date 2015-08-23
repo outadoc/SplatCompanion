88c0cf7c8359b059ca9da3ee50eb5cbe64b829b2
outadoc <outadoc@gmail.com>
2015-08-23 22:24:22 +0200

    Add copyright profile

--- app/src/main/java/outadev/fr/splatcompanion/model/GameModeRegular.java
+++ app/src/main/java/outadev/fr/splatcompanion/model/GameModeRegular.java
@@ -1,3 +1,21 @@
+/*
+ * Splat Companion - Stage rotation schedule viewer for Splatoon(tm)
+ * Copyright (C) 2015  Baptiste Candellier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package outadev.fr.splatcompanion.model;
 
 import outadev.fr.splatcompanion.R;
